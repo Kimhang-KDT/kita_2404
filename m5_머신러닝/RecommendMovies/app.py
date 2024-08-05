d0a92ad8af621107ac0c6b62db38a3de9d107232
Kimhang-KDT <167948223+Kimhang-KDT@users.noreply.github.com>
2024-08-05 20:45:33 +0900

    Add files via upload

--- m5_머신러닝/RecommendMovies/app.py
+++ m5_머신러닝/RecommendMovies/app.py
@@ -1,20 +1,24 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, request
 from utils import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/recommend', methods=['POST'])
+def recommend():
+    userId = request.form['user_id']
     ratings_path = 'ml-latest-small\\ratings.csv'
     movies_path = 'ml-latest-small\movies.csv'
 
     ratings = get_csv_data(ratings_path)
     movies = get_csv_data(movies_path)
-    userId = 1
 
-    top_movies = recommend_movies_for_user(ratings, movies, userId)
+    top_movies = recommend_movies_for_user(ratings, movies, int(userId))
 
-    return render_template('index.html', top_movies=top_movies[:10])
+    return render_template('recommend.html', top_movies=top_movies[:10])
 
 if __name__ == '__main__':
     app.run(debug=True)
